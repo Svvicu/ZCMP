3e54805da85fe626358d1cc4d9083feeccbaedf7
magdabendkowska <magdabendkowska@gmail.com>
2025-11-02 20:36:04 +0100

    Create kalkulator.js

---
+++ src/kalkulator.js
@@ -0,0 +1,28 @@
+export function oblicz(a, b, operacja) {
+  if (isNaN(a) || isNaN(b)) {
+    return { wynik: "Wprowadź obie liczby!", blad: true };
+  }
+
+  let wynik;
+  switch (operacja) {
+    case 'dodaj':
+      wynik = a + b;
+      break;
+    case 'odejmij':
+      wynik = a - b;
+      break;
+    case 'mnoz':
+      wynik = a * b;
+      break;
+    case 'dziel':
+      if (b === 0) {
+        return { wynik: "Błąd: Nie można dzielić przez 0", blad: true };
+      }
+      wynik = a / b;
+      break;
+    default:
+      return { wynik: "Nieznana operacja", blad: true };
+  }
+
+  return { wynik: parseFloat(wynik.toFixed(10)), blad: false };
+}

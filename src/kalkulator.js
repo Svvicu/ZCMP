e2d09069d4a4fbb18b34e63ed0ae017a3e374679
magdabendkowska <magdabendkowska@gmail.com>
2025-11-02 20:36:55 +0100

    Create kalkulator.js

---
+++ src/kalkulator.js
@@ -0,0 +1,17 @@
+export function dodaj(a, b) {
+  return isNaN(a) || isNaN(b) ? "Błąd danych" : a + b;
+}
+
+export function odejmij(a, b) {
+  return isNaN(a) || isNaN(b) ? "Błąd danych" : a - b;
+}
+
+export function mnoz(a, b) {
+  return isNaN(a) || isNaN(b) ? "Błąd danych" : a * b;
+}
+
+export function dziel(a, b) {
+  if (isNaN(a) || isNaN(b)) return "Błąd danych";
+  if (b === 0) return "Błąd: Nie można dzielić przez zero";
+  return a / b;
+}

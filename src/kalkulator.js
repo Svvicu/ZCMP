7fc6136e113721e87cec27c81e370484ec48d10f
magdabendkowska <magdabendkowska@gmail.com>
2025-11-04 11:08:18 +0100

    Update kalkulator.js

--- src/kalkulator.js
+++ src/kalkulator.js
@@ -17,20 +17,6 @@ function dziel(a, b) {
 }
 
 
-function oblicz(a, b, operacja) {
-  switch (operacja) {
-    case "dodaj":
-      return { wynik: dodaj(a, b), blad: false };
-    case "odejmij":
-      return { wynik: odejmij(a, b), blad: false };
-    case "mnoz":
-      return { wynik: mnoz(a, b), blad: false };
-    case "dziel":
-      return { wynik: dziel(a, b), blad: false };
-    default:
-      return { wynik: "Nieznana operacja", blad: true };
-  }
-}
 
 
-module.exports = { oblicz };
+module.exports = { dodaj, odejmij, mnoz, dziel };
